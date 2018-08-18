a9b8f229a415260bd8dacf6d582a3b43b55d261a
Francisco Riadigos <riadigos.fran@gmail.com>
2018-08-18 22:45:00 +0100

    [demo-feature] Added screen On + Deleted release build + Added menu option to clear manually the saved states.

--- demo/src/main/java/com/chattylabs/demo/user/assistant/MainActivity.java
+++ demo/src/main/java/com/chattylabs/demo/user/assistant/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.chattylabs.android.user.interaction.ChatInteractionComponent;
 import com.chattylabs.sdk.android.common.PermissionsHelper;
@@ -28,6 +31,23 @@ public class MainActivity extends DaggerAppCompatActivity {
     @Inject ConversationalFlowComponent voiceComponent;
     private ChatInteractionComponent assistantComponent;
     private RecyclerView view;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                assistantComponent.reset();
+                recreate();
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +97,6 @@ public class MainActivity extends DaggerAppCompatActivity {
             PermissionsHelper.isPermissionGranted(grantResults)) {
             ThreadUtils.newSerialThread().addTask(() -> {
                 assistantComponent = new ChatInteractionHelper(view, voiceComponent).create();
-                assistantComponent.onDone(() -> assistantComponent.reset());
             });
         }
     }
